0fa8f95703c535f29c8ce30b53b06722ca99c67e
radj307 <radj307@gmail.com>
2022-07-12 16:59:08 -0400

    Initial Code Commit

---
+++ ListExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+namespace TypeExtensions
+{
+    /// <summary>Extensions for the generic <see cref="List{T}"/> object.</summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Similar to <see cref="List{T}.AddRange(IEnumerable{T})"/>, but designed for unique elements.<br/>
+        /// Removes elements from <paramref name="l"/> that are <b>not present in <paramref name="other"/></b>.<br/>
+        /// Adds elements from <paramref name="other"/> to <paramref name="l"/> that aren't already present somewhere in the list.<br/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="l">The generic list on which to operate.</param>
+        /// <param name="other">Any enumerable list of type <typeparamref name="T"/>.</param>
+        public static void SelectiveUpdate<T>(this List<T> l, IEnumerable<T> other)
+        {
+            // remove entries that AREN'T present in the incoming list,
+            //  and ARE present in the current list
+            for (int i = l.Count - 1; i >= 0; --i)
+            {
+                if (!other.Any(incomingItem => incomingItem != null && incomingItem.Equals(l[i])))
+                    l.RemoveAt(i);
+            }
+            // add entries that ARE present in the incoming list,
+            //  and AREN'T present in the current list
+            foreach (T incomingItem in other)
+            {
+                if (!l.Any(i => i != null && i.Equals(incomingItem)))
+                    l.Add(incomingItem);
+            }
+        }
+        /// <inheritdoc cref="List{T}.ForEach(Action{T})"/>
+        /// <returns>The resulting <paramref name="list"/>; this can safely be ignored when using this method outside of a pipeline.</returns>
+        public static IList<T> ForEach<T>(this IList<T> list, Action<T> action)
+        {
+            foreach (T? item in list)
+                action(item);
+            return list;
+        }
+        /// <inheritdoc cref="List{T}.ForEach(Action{T})"/>
+        /// <returns>The resulting <paramref name="list"/>; this can safely be ignored when using this method outside of a pipeline.</returns>
+        public static IList ForEach(this IList list, Action<object?> action)
+        {
+            foreach (object? item in list)
+                action(item);
+            return list;
+        }
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,95 @@
+﻿namespace TypeExtensions
+{
+    /// <summary>
+    /// Extensions for string types.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Removes all chars that <paramref name="pred"/> returns true for.
+        /// </summary>
+        public static string RemoveIf(this string s, Predicate<char> pred)
+        {
+            for (int i = s.Length - 1; i >= 0; --i)
+            {
+                if (pred(s[i]))
+                    s = s.Remove(i, i);
+            }
+
+            return s;
+        }
+        /// <summary>
+        /// Removes all preceeding/trailing occurrences of the specified characters from a string.
+        /// </summary>
+        /// <param name="s">The input string.</param>
+        /// <param name="trimChars">Any number of characters in a string.</param>
+        /// <returns>String with all preceeding/trailing characters from trimChars removed.</returns>
+        public static string Trim(this string s, string trimChars)
+            => s.Trim(trimChars.ToCharArray());
+
+        /// <summary>
+        /// Gets the <see langword="char"/> at <paramref name="index"/>, or <paramref name="defaultChar"/> if the index is out of range.
+        /// </summary>
+        /// <param name="s">The string that this extension method was called on.</param>
+        /// <param name="index">The target index within the string to access.</param>
+        /// <param name="defaultChar">A character to return when the index is out-of-range.</param>
+        /// <returns>The character at <paramref name="index"/> in <paramref name="s"/> if the index is within range; otherwise <paramref name="defaultChar"/>.</returns>
+        public static char AtIndexOrDefault(this string s, int index, char defaultChar) => index < s.Length ? s[index] : defaultChar;
+        /// <summary>
+        /// Gets the <see langword="char"/> at <paramref name="index"/>, or the result of the <see langword="default"/> keyword if the index is out of range.
+        /// </summary>
+        /// <returns>The character at <paramref name="index"/> in <paramref name="s"/> if the index is within range; otherwise <see langword="default"/>.</returns>
+        /// <inheritdoc cref="AtIndexOrDefault(string, int, char)"/>
+        public static char AtIndexOrDefault(this string s, int index) => s.AtIndexOrDefault(index, default);
+        /// <summary>
+        /// Check if <paramref name="s"/> equals any of the given <paramref name="compare"/> strings using <paramref name="sCompareType"/> comparison.
+        /// </summary>
+        /// <param name="s">A string.</param>
+        /// <param name="sCompareType">The <see cref="StringComparison"/> to use.</param>
+        /// <param name="compare">Any number of strings to compare to <paramref name="s"/>.</param>
+        /// <returns><see langword="true"/> when <paramref name="s"/> equals at least one of the <paramref name="compare"/> strings; otherwise <see langword="false"/></returns>
+        public static bool EqualsAny(this string s, StringComparison sCompareType, params string[] compare) => compare.Any(c => s.Equals(c, sCompareType));
+        /// <summary>
+        /// Check if <paramref name="s"/> equals any of the given <paramref name="compare"/> strings using <see cref="StringComparison.Ordinal"/> comparison.
+        /// </summary>
+        /// <inheritdoc cref="EqualsAny(string, StringComparison, string[])"/>
+        public static bool EqualsAny(this string s, params string[] compare) => s.EqualsAny(StringComparison.Ordinal, compare);
+        /// <summary>
+        /// Retrieves as much of a substring from this instance as possible without exceeding its boundaries.<br/>
+        /// </summary>
+        /// <remarks>
+        /// This method differs from <see cref="string.Substring(int, int)"/> by not throwing out-of-range exceptions, and instead returns as much of the substring as possible.
+        /// </remarks>
+        /// <param name="s">The <see cref="string"/> instance to operate on.</param>
+        /// <param name="startIndex">The zero-based starting character position of a substring in <paramref name="s"/>.</param>
+        /// <param name="length">The number of characters in the substring.</param>
+        /// <returns>
+        /// A string that is equivalent to the substring of up to length <paramref name="length"/> that begins at <paramref name="startIndex"/> in this instance, or <see cref="string.Empty"/> if <paramref name="startIndex"/> is greater or equal to the length of this instance.
+        /// </returns>
+        public static string TrySubstring(this string s, int startIndex, int length)
+        {
+            if (startIndex > s.Length)
+                startIndex = s.Length;
+            return s.Substring(startIndex, Math.Min(length, s.Length - startIndex));
+        }
+        /// <summary>
+        /// Retrieves a substring from this instance. The substring starts at a specified character position and continues to the end of the string.
+        /// </summary>
+        /// <remarks>
+        /// This method differs from <see cref="string.Substring(int)"/> by not throwing out-of-range exceptions, and instead returns as much of the substring as possible.
+        /// </remarks>
+        /// <param name="s">The <see cref="string"/> instance to operate on.</param>
+        /// <param name="startIndex">The zero-based starting character position of a substring in <paramref name="s"/>.</param>
+        /// <returns>
+        /// A string that is equivalent to the substring that begins at <paramref name="startIndex"/> in <paramref name="s"/>, or <see cref="string.Empty"/> if <paramref name="startIndex"/> is greater or equal to the length of <paramref name="s"/>.
+        /// </returns>
+        public static string TrySubstring(this string s, int startIndex)
+        {
+            if (startIndex > s.Length)
+                startIndex = s.Length;
+#           pragma warning disable IDE0057
+            return s.Substring(startIndex);
+#           pragma warning restore IDE0057
+        }
+    }
+}
